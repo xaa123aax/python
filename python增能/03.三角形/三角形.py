2ef4266388ea4a02ab93c5379b9ad065b9ba43c7
xaa123aax <52443278+xaa123aax@users.noreply.github.com>
2020-05-06 10:59:08 +0800

    Update 三角形.py

--- python增能/03.三角形/三角形.py
+++ python增能/03.三角形/三角形.py
@@ -1,35 +1,28 @@
-# -- coding utf-8 --
-
-Spyder Editor
-
-This is a temporary script file.
+a=int(input("請輸入a的邊長"))
+b=int(input("請輸入b的邊長"))
+c=int(input("請輸入c的邊長"))
 
 
-a=int(input(請輸入a的邊長))
-b=int(input(請輸入b的邊長))
-c=int(input(請輸入c的邊長))
-
-
-if a+bc
-    print(可以圍成三角形)
-    if aa+bbcc
-        if a==b or b==c
-            if a==c
-                print(並且是正三角形)
-            else
-                print(並且是等腰銳角三角形)        
-        else
-            print(並且是銳角三角形)        
-    elif aa+bb==cc
-        if a==b or b==c
-            print(並且是等腰直角三角形)        
-        else
-            print(並且是直角三角形)
-    elif aa+bbcc
-        if a==b or b==c    
-            print(並且是等腰鈍角三角形)
-        else
-            print(並且是鈍角三角形)
+if a+b>c:
+    print("可以圍成三角形")
+    if a*a+b*b>c*c:
+        if a==b or b==c:
+            if a==c:
+                print("並且是正三角形")
+            else:
+                print("並且是等腰銳角三角形")        
+        else:
+            print("並且是銳角三角形")        
+    elif a*a+b*b==c*c:
+        if a==b or b==c:
+            print("並且是等腰直角三角形")        
+        else:
+            print("並且是直角三角形")
+    elif a*a+b*b<c*c:
+        if a==b or b==c:    
+            print("並且是等腰鈍角三角形")
+        else:
+            print("並且是鈍角三角形")
     
-else
-    print(不可以圍成三角形)
+else:
+    print("不可以圍成三角形")
